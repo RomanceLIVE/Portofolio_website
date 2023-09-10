cffa3b7eeebba0fb8cbc15dc7f6a4d8b27c48100
Robert Horvat <robert.horvath93@gmail.com>
2023-09-10 23:16:18 +0300

    update

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+#reminder keys are like ids
+    user_email = st.text_input("Your email address :")
+    message = st.text_area("Your message :")
+    button_submit = st.form_submit_button("Submit")
+    print(button_submit)
+#false if unpressed, true when pressed
+#reminder to put print msgs to verify error, data
+# and how to give good instructions in loops
+    if button_submit:
+        message = message + user_email
+        print("i was pressed")
