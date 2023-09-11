0f63601dde1f82d193dd2648fbbecb74bf721710
Robert Horvat <robert.horvath93@gmail.com>
2023-09-11 21:18:19 +0300

    Update to password security

--- send_email.py
+++ send_email.py
@@ -1,12 +1,17 @@
 import smtplib, ssl
 #python email library
 #ssl secure library
+import  os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "robert.horvath93@gmail.com"
-    password = "occqvucjowjbpujh"
+    password = (os.getenv("PASSWORD"))
+
+    # os.getenv("PASSWORD")) use this line in password =" "  when u share code with ppl
+    # we store our password on Pc using event variables
+    # in case we share the code with ppl
     receiver = "robert.horvath93@gmail.com"
     context = ssl.create_default_context()
 
