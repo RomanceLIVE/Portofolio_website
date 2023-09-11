92e0bd9ddfeb397b6bb96cdd50eb23ef8998d7ab
Robert Horvat <robert.horvath93@gmail.com>
2023-09-11 20:55:44 +0300

    Improved send email

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,16 +1,25 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
 #reminder keys are like ids
     user_email = st.text_input("Your email address :")
-    message = st.text_area("Your message :")
+    raw_message = st.text_area("Your message :")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+
+"""
+# we add #intentional breakline between subject and from
     button_submit = st.form_submit_button("Submit")
     print(button_submit)
 #false if unpressed, true when pressed
 #reminder to put print msgs to verify error, data
 # and how to give good instructions in loops
     if button_submit:
-        message = message + user_email
-        print("i was pressed")
+        send_email(message)
+        st.info("Your email has been sent !")
--- send_email.py
+++ send_email.py
@@ -2,22 +2,21 @@ import smtplib, ssl
 #python email library
 #ssl secure library
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "robert.horvath93@gmail.com"
+    password = "occqvucjowjbpujh"
+    receiver = "robert.horvath93@gmail.com"
+    context = ssl.create_default_context()
 
-username = "robert.horvath93@gmail.com"
-password = "occqvucjowjbpujh"
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        # context(red) from smtplib is an argument
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-receiver = "robert.horvath93@gmail.com"
-context = ssl.create_default_context()
 
-message = """
-Hi !
-How are you?
-Bye !
-"""
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-            #context(red) from smtplib is an argument
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+
+# def send_email(): was made by selecting the code and refactor then extract method
+# send_email() will call that function
